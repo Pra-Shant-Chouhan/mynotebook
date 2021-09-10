2e87a11c42dc45422126f00d020bbb553e26bde8
PRASHANT CHOUHAN <pchouhan122@gmail.com>
2021-09-10 20:19:09 +0530

    Using useContext hook example for practice

--- src/component/About.js
+++ src/component/About.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, {useContext, useEffect} from 'react';
+import noteContext from '../context/notes/noteContext';
 
 const About = () => {
+    const a = useContext(noteContext)
+
+    useEffect(() => {
+        a.update()
+         // eslint-disable-next-line 
+    }, [])
     return (
         <div>
-            This is About Page
+            This is About Page His name is  {a.state.Name} and he is an {a.state.BE} Engineer
         </div>
     )
 }
