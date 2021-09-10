fdbf3233989059c89d75e468a07fbf78c11ac99c
PRASHANT CHOUHAN <pchouhan122@gmail.com>
2021-09-10 19:00:31 +0530

    use react-router-dom and import component  to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import Navbar from './component/Navbar';
+import Home from './component/Home';
+import About from './component/About';
 
 function App() {
   return (
-    <div className="App">
-      <h1> This is MyNoteBook App</h1>
-    </div>
+    <>
+      <Router>
+
+      <Navbar />
+      <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/about">
+            <About/>
+          </Route>
+        </Switch>
+    </Router>
+      
+    </>
   );
 }
 
