06eddca5c5fb336ca1c15f148ce9eeef78b36ce6
PRASHANT CHOUHAN <pchouhan122@gmail.com>
2021-09-11 18:51:17 +0530

    unneccessary hook and comment console

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,12 +1,12 @@
-import React, {}from 'react'
+import React from 'react'
 import { Link, useLocation } from 'react-router-dom'
 
 
 const Navbar = () => {
     let location = useLocation();
-    React.useEffect(() => {
-        console.log(location.pathname)
-     }, [location]);
+    // useEffect(() => {
+    //     console.log(location.pathname)
+    //  }, [location]);
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container-fluid">
