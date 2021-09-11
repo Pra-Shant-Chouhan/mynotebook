4114f8a120906f8869618872131ddc45be81d5c5
PRASHANT CHOUHAN <pchouhan122@gmail.com>
2021-09-11 11:23:50 +0530

    Remove useContext hook example for now

--- src/App.js
+++ src/App.js
@@ -17,14 +17,18 @@ function App() {
         <Router>
 
           <Navbar />
-          <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route exact path="/about">
-              <About />
-            </Route>
-          </Switch>
+          <div className="container">
+
+            <Switch>
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <Route exact path="/about">
+                <About />
+              </Route>
+            </Switch>
+
+          </div>
         </Router>
       </NoteState>
 
