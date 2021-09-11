9ec0feeba6e5685a980f783d04fee9fe7250f714
PRASHANT CHOUHAN <pchouhan122@gmail.com>
2021-09-11 11:25:58 +0530

    Remove useContext hook

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -1,25 +1,12 @@
 import NoteContext from "./noteContext"
-import { useState } from "react"
+// import { useState } from "react"
 const NoteState = (props) => {
-    const shi = {
-        "Name": "Bitt2",
-        "BE": "EC"
-    }
-    const [state, setState] = useState(shi)
-    const update = () => {
-        setTimeout(() => {
-            setState({
-                "Name": "Mon2",
-                "BE": "CE"
-            })
 
-        }, 1000);
-    }
     return (
-        <NoteContext.Provider value={{ state, update }}>
+        <NoteContext.Provider value={{}}>
             {props.children}
         </NoteContext.Provider>
     )
+    }
 
-}
 export default NoteState;
