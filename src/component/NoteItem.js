6ad237d2441e59dfa6bd41625e38c38cb0e4273d
PRASHANT CHOUHAN <pchouhan122@gmail.com>
2021-09-12 13:19:26 +0530

    tag line added

--- src/component/NoteItem.js
+++ src/component/NoteItem.js
@@ -4,16 +4,17 @@ import noteContext from '../context/notes/noteContext'
 const NoteItem = (props) => {
     const context = useContext(noteContext);
     const { deleteNote } = context;
-    const { note } = props
+    const { note,updateNote } = props
     return (
         <div className = "col-md-3">
 
             <div className="card my-3" >
                 <div className="card-body">
                 <h5 className="card-title">{note.title}</h5>
+                <p className="card-text">{note.tag}</p>
                 <p className="card-text">{note.description}</p>
                     <i className="fas fa-trash-alt mx-4" onClick ={()=>{ deleteNote(note._id)}}></i>
-                    <i className="fas fa-edit mx-4"></i>
+                    <i className="fas fa-edit mx-4" onClick ={()=>{updateNote(note)}}></i>
                 </div>
             </div>
         </div>
