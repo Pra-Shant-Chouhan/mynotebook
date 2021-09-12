d1a6f123e6c25b68b24656d3b5194c2581f923cc
PRASHANT CHOUHAN <pchouhan122@gmail.com>
2021-09-12 15:20:17 +0530

    disabled buttun functionality added

--- src/component/AddNote.js
+++ src/component/AddNote.js
@@ -5,10 +5,12 @@ import noteContext from '../context/notes/noteContext';
 const AddNote = () => {
     const context = useContext(noteContext);
     const { addNote } = context;
-    const [note, setNote] = useState({title: " ", description: "", tag : ""})
+    const [note, setNote] = useState({ title: " ", description: "", tag: "" })
+    
     const ClickInput = (e) => {
         e.preventDefault();
         addNote(note.title, note.description, note.tag);
+        setNote({ title: " ", description: "", tag: "" }); // After adding note make filed of title, description and tag empty
     }
 
     const onChange = (e) => {
@@ -20,19 +22,19 @@ const AddNote = () => {
             <form action="">
                 <div className="mb-3">
                     <label htmlFor="title" className="form-label">Title</label>
-                    <input type="text" className="form-control" id="title"  name ="title"aria-describedby="emailHelp" onChange={onChange}/>
+                    <input type="text" className="form-control" id="title" name="title" aria-describedby="emailHelp" onChange={onChange} minLength={5} required value={ note.title}/>
         
                 </div>
                 <div className="mb-3">
                     <label htmlFor="tag" className="form-label">Tag</label>
-                    <input type="text" className="form-control" id="tag" name="tag" onChange={onChange}/>
+                    <input type="text" className="form-control" id="tag" name="tag" onChange={onChange} minLength={5} required value={ note.tag}/>
                 </div>
                 <div className="mb-3">
                     <label htmlFor="description" className="form-label">Description</label>
-                    <input type="text" className="form-control" id="description" name="description" onChange={onChange}/>
+                    <input type="text" className="form-control" id="description" name="description" onChange={onChange} minLength={5} required value={ note.description}/>
                 </div>
                 
-                <button type="submit" className="btn btn-primary" onClick= {ClickInput}>Add Note</button>
+                <button disabled={note.title.length<5||note.description.length<5} type="submit" className="btn btn-primary" onClick= {ClickInput}>Add Note</button>
             </form>
             
         </div>
