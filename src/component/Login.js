8dac63537aa75a9411aed00b5511c87cbc3fc91c
PRASHANT CHOUHAN <pchouhan122@gmail.com>
2021-09-13 12:18:10 +0530

    use login form and and using useHistory hook we go to user data page and save it on localstorage

---
+++ src/component/Login.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react'
+import { useHistory } from 'react-router-dom'
+const Login = () => {
+    const [credential, setCredential] = useState({ email: "", password: "" })
+    let history = useHistory();
+    const clickonSubmit = async (e) => {
+        e.preventDefault();
+        const response = await fetch("http://localhost:5000/api/auth/login", {
+            method: 'POST', // *GET, POST, PUT, DELETE, etc.
+
+            headers: {
+                'Content-Type': 'application/json'// 'Content-Type': 'application/x-www-form-urlencoded',
+
+            },
+            body: JSON.stringify({ email: credential.email, password: credential.password })
+        });
+        const json = await response.json(); // parses JSON response into native JavaScript objects
+        console.log(json);
+        //Login failed Alert 
+        if (json.success) {
+            //Save the Auth token and redirect
+            localStorage.setItem('token', json.authtoken)
+            history.push("/");
+        }
+        else {
+            alert("invlaid credentials")
+        }
+
+    }
+    const onChange = (e) => {
+        setCredential({ ...credential, [e.target.name]: e.target.value })
+    }
+
+    return (
+        <div>
+            <form onSubmit={clickonSubmit}>
+                <div className="mb-3">
+                    <label htmlFor="email" className="form-label">Email address</label>
+                    <input type="email" className="form-control" id="email" name="email" aria-describedby="emailHelp" value={credential.email} onChange={onChange} />
+                    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="password" className="form-label">Password</label>
+                    <input type="password" className="form-control" id="password" name="password" value={credential.password} onChange={onChange} />
+                </div>
+
+                <button type="submit" className="btn btn-primary" onSubmit={clickonSubmit}>Submit</button>
+            </form>
+        </div>
+    )
+}
+
+export default Login
