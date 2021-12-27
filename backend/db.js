c040b55c698fe80eb4f93cb02e913fd15ca75723
PRASHANT CHOUHAN <pchouhan122@gmail.com>
2021-12-27 23:02:15 +0530

    last try

--- backend/db.js
+++ backend/db.js
@@ -1,4 +1,5 @@
 import { connect } from 'mongoose';
+ 
 // const mongoURI = "mongodb://localhost:27017/inotebook?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false" 
 const mongoURI = "mongodb+srv://Bitt2boss:Bitt2boss@my-note-bookk.sjola.mongodb.net/inotebook?retryWrites=true&w=majority"
 
--- backend/index.js
+++ backend/index.js
@@ -15,6 +15,14 @@ app.use(express.json())
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/notes', require('./routes/notes'))
 
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static("client/build"));
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+
+}
 
 
 app.listen(process.env.PORT||port, () => {
